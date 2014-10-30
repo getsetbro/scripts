2a33311024fd4a1198ff103fe13dc734ebde8999
Seth Broweleit <hello[@]getsetbro[.]com>
2014-10-30 14:37:18 -0500

    v1a

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,12 @@
-console.log('\'Allo \'Allo!');
+$.getJSON(url, function(data) {
+'use strict';
+    var items = [];
+    $.each(data, function(key, val) {
+        items.push('<li id="' + key + '"><span>' + val + '</span></li>');
+    });
+
+    $('<ol/>', {
+        class: 'my-new-list',
+        html: items.join('')
+    }).appendTo('body');
+});
